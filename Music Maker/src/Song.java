8208b6b6725ef7a4d8213eec4af7b06e50b2e319
LAPTOP-B2LB17ER\brend <bricha40@msudenver.edu>
2017-04-25 09:15:06 -0600

    song constructor and piano frequencies reference image

--- Music Maker/src/Song.java
+++ Music Maker/src/Song.java
@@ -4,26 +4,36 @@ public class Song {
     
     //music stuff
     private Part[] parts;
+    private int numParts;
+    private int maxParts = 3;
     private float length;
     private TSigChange[] tSigChanges;
     private TempoChange[] tempoChanges;
     private int numBars;
+    private int maxBars = 50;
 	
-	protected static Random rand = new Random();	
+    protected static Random rand = new Random();	
 	
     //GA stuff
     protected int fitness;
-	protected static final double mutationProb = 0.01,
-			crossoverProb = 1.0;
-	private int crossoverPoint;
+    protected static final double mutationProb = 0.01,
+                                  crossoverProb = 1.0;
+    private int crossoverPoint;
 
 /////////////////////////////////////////////////////////////
 //constructors:
 //the only difference between the 2 is that 
-//the constructor with no arguments makes a random formula,
-//while the 2 argument constructor crosses 2 parents' formulas to make its own
+//the constructor with no arguments makes a song,
+//while the 2 argument constructor crosses 2 parents' genes to make a song
 	public Song(){
-
+            numBars = rand.nextInt(maxBars-10)+10;
+            
+            numParts = rand.nextInt(maxParts-1)+1;
+            parts = new Part[numParts];
+            for(int i=0; i<numParts; i++){
+                parts[i] = new Part();
+            }
+            
 	}
 	
 	public Song(Song a, Song b){
